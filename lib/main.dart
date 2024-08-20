f98a90c8e6a9a8cc279971f35b298cfba649a9f9
Omar Assem <omar@192.168.1.6>
2024-08-20 20:48:47 +0300

    Initialize project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:news/home_screen.dart';
+
+void main() {
+  runApp(const NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        HomeScreen.routeName: (_) => const HomeScreen(),
+      },
+    );
+  }
+}
